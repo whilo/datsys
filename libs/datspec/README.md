a0b27354c7bcebcdfa044059d1f60897ba0c9f01
Christopher T Small <metasoarous@gmail.com>
2016-06-21 11:31:38 -0700

    Completing dat* lib extraction

--- libs/datspec/README.md
+++
@@ -1,9 +0,0 @@
-# DatProtocols
-
-Protocols for the DatSys architecture.
-
-See `src/dat/protocols.cljc` for documentation.
-
-
-
-
--- project.clj
+++ project.clj
@@ -1,17 +1,23 @@
 (defproject datsys "1.0.0"
-  :description "On opinionated (+ clj cljs datomic datascript (reagent react) web development framework" ;;should this be "an" or "un"?
+  :description "(+ clj cljs datomic datascript frp) web development framework" ;;should this be "an" or "un"?
   :url "https://github.com/metasoarous/datsys"
   :license {:name "Eclipse Public License"
-  ;; Change to datsys eventually...
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0-alpha6"]
                  [org.clojure/clojurescript "1.9.36"]
                  [org.clojure/core.async "0.2.382"]
+                 [org.clojure/tools.logging "0.3.1"] ;; Should remove this for timbre
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.typed "0.3.23"]
+                 ;; Datsys things
+                 [datsync "0.0.1-alpha1"]
+                 [datview "0.0.1-alpha1"]
+                 [datspec "0.0.1-alpha1"]
+                 [datreactor "0.0.1-alpha1"]
+                 ;; Other stuff (should try to clean things up once in main project)
                  [com.stuartsierra/component "0.3.1"]
                  [environ "1.0.3"]
-                 [ch.qos.logback/logback-classic "1.1.7"]
-                 [org.clojure/tools.logging "0.3.1"]
                  [slingshot "0.12.2"]
                  [ring/ring-core "1.5.0"]
                  [ring/ring-defaults "0.2.1"]
@@ -22,23 +28,20 @@
                  [com.cognitect/transit-cljs "0.8.237"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [testdouble/clojurescript.csv "0.2.0"]
+                 [datascript "0.15.0"]
+                 [posh "0.3.5"]
                  [reagent "0.6.0-rc"]
                  [re-frame "0.7.0"]
                  [org.webjars/bootstrap "3.3.5"]
-                 [posh "0.3.5"]
                  [re-com "0.8.3"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
                  [prismatic/schema "1.1.2"]
-                 [org.clojure/core.typed "0.3.23"]
-                 [datsync "0.0.1-SNAPSHOT"]
-                 [datascript "0.15.0"]
                  [io.rkn/conformity "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
                  [com.taoensso/timbre "4.4.0"]
                  [com.taoensso/encore "2.56.0"]
                  [com.taoensso/sente "1.8.1" :exclusions [org.clojure/tools.reader]]
                  ;;For the free version of Datomic
-                 [com.datomic/datomic-free "0.9.5372"
-                  :exclusions [joda-time]]]
+                 [com.datomic/datomic-free "0.9.5372" :exclusions [joda-time]]]
   :plugins [[lein-cljsbuild "1.1.1"]]
   ;; For Datomic Pro uncomment the following and set the DATOMIC_USERNAME and DATAOMIC_PASSWORD environment
   ;; variables of the process in which you run this program to those matching your Datomic Pro account. You'll
@@ -92,7 +95,9 @@
                                    taoensso.timbre/trace
                                    taoensso.timbre/spy]}
                     :source-paths ["dev"]
-                    :resource-paths ^:replace ["resources" "dev-resources" "resources-index/dev"]
+                    ;; libs/datsync/resources is important here; It's lib code need access to it's resources
+                    ;; dir in dev
+                    :resource-paths ^:replace ["resources" "libs/datsync/resources" "dev-resources" "resources-index/dev"]
                     :cljsbuild
                     {:builds
                      {:client {:source-paths ["dev"]
